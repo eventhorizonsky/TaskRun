6f90091f7ceabcb786e9c7674d588c2bd6f1c871
ezsky111 <ezsky111@gmail.com>
2025-12-30 09:31:05 +0000

    【功能】(后端): 更新 SQLAlchemy 连接配置，支持从环境变量读取配置

--- backend/funboost_config.py
+++ backend/funboost_config.py
@@ -58,7 +58,7 @@ class BrokerConnConfig(DataClassBase):
         "security_protocol": "SASL_PLAINTEXT",
     }
 
-    SQLACHEMY_ENGINE_URL = 'mysql://root:xyztxdys@172.17.0.1:3306/test'
+    SQLACHEMY_ENGINE_URL = os.getenv('SQLACHEMY_ENGINE_URL', 'mysql://root:xyztxdys@172.17.0.1:3306/test')
 
     # 如果broker_kind 使用 peewee 中间件模式会使用mysql配置
     MYSQL_HOST = '127.0.0.1'
